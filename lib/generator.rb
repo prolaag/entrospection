7bcd0340bde44a6cf96d6b167b50b9f7a1228c1e
kedji <kedjikeen@gmail.com>
2015-04-15 23:36:42 -0400

    using aes_ecb.rb as a model for what generator objects ought to look like

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,32 +1,86 @@
 #!/usr/bin/env ruby
 # encoding: ASCII-8BIT
 
-module Generator
+class Generator
 
-  PROJ_ROOT = File.expand_path('../..', __FILE__)
-  GENERATOR_DIR = File.expand_path('lib/generators', PROJ_ROOT)
+  def initialize(limit = Float::INFINITY)
+    @bytes_remaining = limit
+    @buff = ''
+  end
+  attr_reader :bytes_remaining
 
-  def self.list_generators
-    Dir.glob("#{GENERATOR_DIR}/*.yaml").map do |file|
-      file.sub(/\.yaml$/, '')
+  # Alternate constructor that reads the byte limit (if present) from ARGV.
+  # Writes output to dst, or returns the generator if dst == nil
+  def self.run(dst = $stdout)
+    limit = Float::INFINITY
+    if ARGV[0].to_i != 0
+      unit = { 'k' => 1024, 'm' => 2**20, 'g' => 2**30 }[ARGV[0][-1].downcase]
+      limit = ARGV[0].to_i * (unit || 1)
     end
+    generator = self.new(limit)
+    generator.pipe_to(dst) if dst
+    generator
   end
 
-  def self.description(name)
-    file = File.expand_path("#{name}.yaml", GENERATOR_DIR)
-    fail "#{file} does not exist" unless File.exist? file
-    YAML.load(File.read(file))['description']
+  def summary
+    "No summary has been provided for #{self.class}"
   end
 
-  def self.summary(name)
-    file = File.expand_path("#{name}.yaml", GENERATOR_DIR)
-    fail "#{file} does not exist" unless File.exist? file
-    YAML.load(File.read(file))['summary']
+  def description
+    "No description has been provided for #{self.class}"
   end
 
-  def self.run(opts)
-    cmd = "#{File.expand_path("#{opts[:generator]}.rb", GENERATOR_DIR)} #{opts[:limit]}"
-    exec(cmd)
+
+  ##  Mimic minimal set of IO behaviors  ##
+
+  def read(bytes)
+    ret = ''
+
+    # Ensure we don't go past our byte limit
+    if bytes > @bytes_remaining
+      return nil if @bytes_remaining == 0
+      bytes = @bytes_remaining
+    end
+    @bytes_remaining -= bytes
+
+    # Read whole buffs at a time, then take bytes as needed from the last buff
+    while bytes - ret.length > @buff.length
+      ret << @buff
+      @buff = next_chunk
+    end
+    ret << @buff.slice!(0, bytes - ret.length)
+  end
+  alias :readpartial :read
+
+  def readchar
+    return nil if @bytes_remaining == 0
+    @bytes_remaining -= 1
+    @buff = next_chunk if @buff.empty?
+    @buff.slice!(0)
+  end
+
+  def readbyte
+    read(1).ord
+  end
+
+  def each
+    yield readchar while @bytes_remaining > 0
+  end
+  alias :each_char :each
+
+  def each_byte
+    yield readbyte while @bytes_remaining > 0
+  end
+
+  # Write to the given IO object up to the provided byte limit bytes.
+  # Return on SIGPIPE or when the byte limit is reached.
+  def pipe_to(io)
+    begin
+      io.print(read(8192)) while @bytes_remaining > 0
+    rescue Errno::EPIPE
+      return $!
+    end
+    nil
   end
 
 end
--- lib/generators/aes_ecb.rb
+++ lib/generators/aes_ecb.rb
@@ -3,24 +3,27 @@
 # This test generates a pseudo-random sequence by encrypting a simple counter
 # with AES in ECB mode.
 
-require_relative 'generator.rb'
+require_relative '../generator.rb'
 require 'openssl'
 
-def ecb(key, text)
-  (aes = OpenSSL::Cipher::Cipher.new('aes-256-ecb').send(:encrypt)).key = key
-  aes.update(text) << aes.final
+class AesEcbGenerator < Generator
+
+  def ecb(key, text)
+    (aes = OpenSSL::Cipher::Cipher.new('aes-256-ecb').send(:encrypt)).key = key
+    aes.update(text) << aes.final
+  end
+
+  def initialize(*args)
+    super(*args)
+    @key = "\0" * 32
+    @i = 0
+  end
+
+  def next_chunk
+    @i += 1
+    ecb(@key, [@i].pack('Q>'))
+  end
+
 end
 
-limit = ARGV.first.to_i
-bytes_max = limit ? limit : 0
-bytes = 0
-
-key = "\0" * 32
-i = 0
-loop do
-  data = ecb(key, [i].pack('Q>'))
-  gprint data
-  bytes += data.length
-  i += 1
-  break if bytes > bytes_max
-end
+AesEcbGenerator.run if __FILE__ == $0
