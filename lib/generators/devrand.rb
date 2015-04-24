f9a66e238d7a164913b01c145d2e38a2dd4e6294
Jeremy Moles <cubicool@gmail.com>
2015-04-24 13:02:00 -0400

    Adds support for reading from /dev/random.
    This patch adds a generate that reads 128 bytes per chunk from the
    /dev/random device node. Depending on what version of the kernel you're
    using (and whether you use IvyBridge/Broadwell hardware), this may not
    be entirely "random" afterall.

---
+++ lib/generators/devrand.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+# Reads a 128 bytes per chunk request at a time from the /dev/random device
+# node (which will block until there is proper entropy).
+
+require_relative '../generator.rb'
+
+class DevRandomGenerator < Generator
+  def initialize(*args)
+    super(*args)
+    @random = File.open("/dev/random")
+  end
+
+  def finalize(*args)
+    @random.close()
+  end
+
+  def self.summary
+    "Reads 128 bytes at a time from the /dev/random device file"
+  end
+
+  def self.description
+    desc = <<-DESC_END
+      This generator uses the /dev/random device to generate completely
+      random (inasmuch as the kernel and your hardware provide) noise. However,
+      there is some concern that if your kernel enables RDRAND support for
+      /dev/random (and the rumors around Intel introducing a purposeful
+      backdoor are ture), it may not be entirely non-deterministic afterall!
+    DESC_END
+    desc.gsub(/\s+/, " ").strip
+  end
+
+  def next_chunk
+    return @random.read(128)
+  end
+end
+
+DevRandomGenerator.run if __FILE__ == $0
+
