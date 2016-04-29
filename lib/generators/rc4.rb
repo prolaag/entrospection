4b54cbf2cfae4cacc14812ed805f2efcab766970
TheyDroppedMe <TheyDroppedMe@users.noreply.github.com>
2016-04-28 21:17:48 -0400

    Cleaning up the code and fixing comments

--- lib/generators/rc4.rb
+++ lib/generators/rc4.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 
-# This implements a 64-bit Xorshift* PRNG
+# This implements the RC4 stream cipher
 # Code adapted from https://en.wikipedia.org/wiki/RC4#Pseudo-random_generation_algorithm_.28PRGA.29
 # License for the above at https://en.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported
+# I make no guarantees that this is in any way correct...
 
 require_relative '../generator.rb'
 
@@ -10,22 +11,17 @@ class RC4 < Generator
 
   def initialize(*args)
     super(*args)
-    # Here @r is the random number generated
+    # Here @r is the random number generated - i, j, and s are used to store RC4 states
     @r = 0
     @i = 0
     @j = 0
-    @k = 0
     # Key length of 5 = 40 bits
-    keylen = 5
-    key = [1, 2, 3, 4, 5]
+    key = *(1..5)
     # Initialize the array
-    @s = Array.new
-    for x in 0..255
-      @s[x] = x
-    end
+    @s = *(0..255)
     y = 0
     for x in 0..255
-      y = (y + @s[x] + key[x % keylen]) % 256
+      y = (y + @s[x] + key[x % key.length]) % 256
       @s[x], @s[y] = @s[y], @s[x]
     end
   end
@@ -36,8 +32,8 @@ class RC4 < Generator
 
   def self.description
     desc = <<-DESC_END
-    This generates an 8-bit pseudo-random sequence using RC4. This uses a 40-bit key of '12345'. Definitely not 
-    cryptographically secure.
+    This generates an 8-bit pseudo-random sequence using an RC4 stream cipher using a hardcoded 40-bit key. Not 
+    cryptographically secure, but perhaps a little interesting.
     DESC_END
     desc.gsub(/\s+/, " ").strip
   end
